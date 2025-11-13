b43f666718d3ec82d80d0f605b38d1792002d36c
Marcel Niemiec <marcel.niemiec6@gmail.com>
2025-11-13 21:17:03 +0100

    chore: dirs cleaning

---
+++ app/cms-fs/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from 'next/navigation';
+
+export default function CmsFsPage() {
+  const externalUrl = 'https://www.sanity.io/@oZO3IeHMY/studio/jgwssc1wv9uw7ydu1olmi3mb/default/structure/hero;87598059-2d59-4cef-a6c3-68e179dbdb6b';
+  redirect(externalUrl);
+}
